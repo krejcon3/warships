8133c3ca111d6294ad57265d5bb478fadf358ceb
Ondřej Krejčíř <krejcon3@gmail.com>
2018-02-28 16:46:36 +0100

    Add files via upload

--- src/client/script.js
+++ src/client/script.js
@@ -2,12 +2,14 @@ var Game = function (i_name) {
   var size = 10;
 	var name = i_name;
   var key = null;
+  var flag = true;
+  var won = false;
 
 	this.register = function() {
 		console.log("Register player: " + name);
 		$.get("http://warships.ondrejkrejcir.cz/register.php", { name: name }, function (data, status) {
-      			key = data.key;
-      			console.log(key);
+      key = data.key;
+      console.log(key);
 		});
 	}
   
@@ -21,18 +23,42 @@ var Game = function (i_name) {
   var playground = createArray();
   
   var shoot = function() {
+    if (!flag) {
+      return;
+    }
     var x = Math.floor(Math.random() * 10); 
     var y = Math.floor(Math.random() * 10);
     console.log("Shooting at [" + x + "," + y + "]");
     
+    flag = false;
     $.get("http://warships.ondrejkrejcir.cz/shoot.php", { hash: key, x: x, y: y }, function (data, status) {
       playground[data.x][data.y] = data.hit;
-	}).always(function() {flag = true; });
+  	}).always(function() { flag = true; });       
   };
+  
+  var check = function() {
+    var hits = 0;
+    for (var x = 0; x < playground.length; x++) {
+      for (var y = 0; y < playground.length; y++) {
+         if (playground[x][y]) {
+            hits++;
+         } 
+      }
+    }
+    if (hits < 4) return; 
+    $.get("http://warships.ondrejkrejcir.cz/check.php", { hash: key }, function (data, status) {
+         if (status == "OK") {
+            won = true;
+         } 
+  	});
+  }; 
   
   var drawPlayground = function() {
       var canvas = document.getElementById("canvas");
       var context = canvas.getContext("2d");
+      if (won) {
+        canvas.style.backgroundColor = "green";
+      }
       context.clearRect(0, 0, canvas.width, canvas.height);
       for (var i = 1; i < size; i++) {
         context.beginPath();
@@ -67,13 +93,14 @@ var Game = function (i_name) {
   }; 
   
   this.turn = function() {
-     shoot();
-     drawPlayground();
+    check();
+    shoot();
+    drawPlayground();
   }
 };
 
 $(document).ready(function() {
-	var game = new Game("KrejÃ¨Ã­Ã¸");
+	var game = new Game("Krejèíø");
 	game.register();
   setInterval(game.turn, 2000);
 });
